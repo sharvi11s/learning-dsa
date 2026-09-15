876cebc44028aa06688ca83b9c1e07e67ac51a31
sharvi11s <sharvis4251csefy2@gmail.com>
2026-09-15 21:51:26 +0530

    modified code for traversal types in a binary tree

--- binaryTrees.cpp
+++ binaryTrees.cpp
@@ -86,6 +86,40 @@ void levelOrder(Node* root){
     cout << endl;
 }
 
+// level order traversal with a null value
+void levelOrderWithNull(Node* root){
+    queue<Node*> q;
+
+    q.push(root);
+    q.push(NULL);
+
+    while(q.size() > 0){
+        Node* curr = q.front();
+        q.pop();
+
+        if(curr == NULL){
+            if(!q.empty()) {
+                cout << endl;
+                q.push(NULL);
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        cout << curr->data << " ";
+
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+        if(curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+
+    cout << endl;
+}
+
 int main(){
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
@@ -102,6 +136,9 @@ int main(){
     cout << endl;
 
     levelOrder(root);
+    cout << endl;
+    
+    levelOrderWithNull(root);
     cout << endl;
     
     return 0;
