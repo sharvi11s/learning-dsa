bbac562fd419d6ed52779b0394659902e8dfd961
sharvi11s <sharvis4251csefy2@gmail.com>
2026-09-15 20:43:46 +0530

    added code for traversal types in a binary tree

--- binaryTrees.cpp
+++ binaryTrees.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 class Node{
@@ -29,11 +30,79 @@ Node* buildTree(vector<int> preorder){
     return root;
 }
 
+// preorder traversal
+void preOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+// inorder traversal
+void inOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+// post order traversal
+void postOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+// level order traversal
+void levelOrder(Node* root){
+    queue<Node*> q;
+
+    q.push(root);
+
+    while(q.size() > 0){
+        Node* curr = q.front();
+        q.pop();
+
+        cout << curr->data << " ";
+
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+        if(curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+
+    cout << endl;
+}
+
 int main(){
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
     Node* root = buildTree(preorder);
     cout << root->data << endl;
+
+    preOrder(root);
+    cout<< endl;
+
+    inOrder(root);
+    cout << endl;
+
+    postOrder(root);
+    cout << endl;
+
+    levelOrder(root);
+    cout << endl;
     
     return 0;
 }
